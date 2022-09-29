de359b8edcd4106439ada8a8319a3611c6f7d17c
Nikhil <nikhilshetye24@gmail.com>
2022-09-28 17:14:17 -0700

    rebuild for erros in deploy

--- src/components/RecipesList/RecipesList.js
+++ src/components/RecipesList/RecipesList.js
@@ -6,7 +6,7 @@ function RecipesList({recipeList,activeRecipeId}) {
   return (
     <ListGroup>
       {recipeList.map((e)=>{
-        return (<ListGroup.Item as={Link} to={`${e.id}`} variant={activeRecipeId == e.id ? 'success' : ''} key={e.id}>
+        return (<ListGroup.Item as={Link} to={`${e.id}`} variant={activeRecipeId === e.id ? 'success' : ''} key={e.id}>
           {e.title}
         </ListGroup.Item>)
       })}
